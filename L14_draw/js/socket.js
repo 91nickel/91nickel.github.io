ca46cec226c4a3e371a28e80c9b969f3d0b24b13
91nickel <91nickel@gmail.com>
2019-06-06 00:26:02 +0300

    lesson14

--- L14_draw/js/socket.js
+++ L14_draw/js/socket.js
@@ -1,20 +1,27 @@
 'use strict';
 
 var connection = new WebSocket('wss://neto-api.herokuapp.com/draw');
+connection.binaryType = 'arraybuffer';
+
 connection.addEventListener('open', (e) => {
     console.log('Вебсокет-соединение открыто');
+
+    connection.addEventListener('message', (e) => {
+        console.log('Получено сообщение от сервера');
+        console.log(e.data.byteLength);
+    })
+
+    window.editor.addEventListener('update', (e) => {
+        console.log('Сработало событие update');
+
+        const canvas = e.canvas;
+        const ctx = canvas.getContext('2d');
+        const image = ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
+        const binary = Uint8Array.from(image.data);
+        connection.send(binary.buffer);
+    })
+
 });
 
-connection.addEventListener('message', (e)=>{
-    console.log(e);
-})
 
-window.editor.addEventListener('update', (e) => {
-    console.log('Сработало событие update');
-    console.log(e.canvas);
 
-    const ctx = e.canvas.getContext('2d');
-    const image = ctx.getImageData(0, 0, 650, 800);
-    const binary = Uint8Array.from(image.data);
-    connection.send(binary.buffer);
-})
