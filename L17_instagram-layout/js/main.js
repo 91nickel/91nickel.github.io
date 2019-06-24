9ea891d0141010424238bf4e0ef87001e1906c41
91nickel <43931371+91nickel@users.noreply.github.com>
2019-06-24 19:15:33 +0300

    lesson17

--- L17_instagram-layout/js/main.js
+++ L17_instagram-layout/js/main.js
@@ -1,12 +1,12 @@
-const addClass = ( className, context ) => context.classList.add( className ),
-  removeClass = ( className, context ) => context.classList.remove( className ),
-  hasClass = ( className, context ) => context.classList.contains( className );
+const addClass = (className, context) => context.classList.add(className),
+  removeClass = (className, context) => context.classList.remove(className),
+  hasClass = (className, context) => context.classList.contains(className);
 class iLayout {
-  constructor( container ) {
+  constructor(container) {
     this.container = container;
-    this.positionsContainer = container.querySelector( '.layout__positions' );
-    this.actionButton = container.querySelector( '.layout__button' );
-    this.result = container.querySelector( '.layout__result' );
+    this.positionsContainer = container.querySelector('.layout__positions');
+    this.actionButton = container.querySelector('.layout__button');
+    this.result = container.querySelector('.layout__result');
     this.layout = {
       left: null,
       top: null,
@@ -15,7 +15,66 @@ class iLayout {
     this.registerEvents();
   }
   registerEvents() {
+    document.addEventListener('dragover', (e) => {
+      e.preventDefault();
+    })
+    document.addEventListener('drop', this.addImage.bind(this));
+    this.actionButton.addEventListener('click', this.createCollage.bind(this))
+  }
+  addImage(e) {
+    e.preventDefault();
+    let file = e.dataTransfer.files[0];
+    const container = e.target;
+    const imageTypeRegExp = /^image\//;
+
+    try {
+      if (!hasClass('layout__item', container)) {
+        throw ('Ошибка контейнера!');
+      }
+
+      if (!imageTypeRegExp.test(file.type)) {
+        throw ('Ошибка типа файла!');
+      }
+
+      console.log('Файл прошел проверку');
+    } catch (e) {
+      file = null;
+      console.log(e);
+    }
+    console.log(e);
+    if (file) {
+      const img = document.createElement('img');
+      addClass('layout__image', img);
+      img.src = URL.createObjectURL(file);
+      container.innerHTML = '';
+      container.appendChild(img);
+    }
+  }
+  createCollage(e) {
+    if(this.positionsContainer.querySelectorAll('img').length !== 3) {
+      console.error('Недостаточно данных для формирования изображения');
+      return;
+    }
+
+    const canvas = document.createElement('canvas');
+
+    const imageLeft = this.positionsContainer.querySelector('.layout__item_left img');
+    const imageTop = this.positionsContainer.querySelector('.layout__item_top img');
+    const imageBottom = this.positionsContainer.querySelector('.layout__item_bottom img');
+
+    canvas.width = imageLeft.parentElement.clientWidth + imageTop.parentElement.clientWidth;
+    canvas.height = imageLeft.parentElement.clientHeight;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(imageLeft, 0, 0);
+    ctx.drawImage(imageTop, imageLeft.parentElement.clientWidth, 0);
+    ctx.drawImage(imageBottom, imageLeft.parentElement.clientWidth, imageTop.parentElement.clientHeight);
+
+    const img = document.createElement('img');
+    img.src = canvas.toDataURL();
+    this.container.appendChild(img);
+    this.result.innerText = img.src;
   }
 }
 
-new iLayout( document.getElementById( 'layout' ));
+new iLayout(document.getElementById('layout'));
--- L17_sprite-generator/js/main.js
+++ L17_sprite-generator/js/main.js
@@ -1,6 +1,8 @@
 const prop = (data, name) => data.map(item => item[name]),
   summ = data => data.reduce((total, value) => total + value, 0);
 
+const body = document.querySelector('body');
+
 class SpriteGenerator {
   constructor(container) {
     this.uploadButton = container.querySelector('.sprite-generator__upload');
@@ -16,34 +18,86 @@ class SpriteGenerator {
     this.registerEvents();
   }
   registerEvents() {
-    this.submitButton.addEventListener('click', this.loadFiles.bind(this));
-
+    this.submitButton.addEventListener('click', this.createSprite.bind(this));
+    this.uploadButton.addEventListener('change', this.loadFiles.bind(this));
   }
   loadFiles(e) {
-    console.log('Load Files');
-    console.log(this.uploadButton);
+    console.log('LOAD FILES');
     const result = this.imageElement;
-    const files = Array.from(this.uploadButton.files);
-    console.log(files[0]);
-    const canvas = document.createElement('canvas');
-    //canvas.width = 256;
-    //canvas.height = 256;
-    const img = document.createElement('img');
-    img.src = URL.createObjectURL(files[0]);
+    const images = Array.from(this.uploadButton.files).map((el) => {
+      const image = document.createElement('img');
+      image.src = URL.createObjectURL(el);
+      URL.revokeObjectURL(el);
+      image.style.width = '256px';
+      image.style.height = '256px';
+      this.images.push(image);
+      return image;
+    });
+    
 
-    //console.log(document.children[0].children[1]);
-    document.children[0].children[1].appendChild(img);
-    document.children[0].children[1].appendChild(canvas);
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-
-    console.log(canvas);
-    console.log(canvas.toDataURL())
-    result.src = canvas.toDataURL();
-    console.log(result);
-
-    //document.children[1].appendChild(img);
+    this.imagesCountContainer.innerText = this.images.length;
   }
+  createSprite() {
+    console.log('CREATE SPRITE');
+    if(this.images.length === 0){
+      console.log('Отказ в операции');
+      return;
+    }
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 256 * matrix(this.images.length).width;
+    canvas.height = 256 * matrix(this.images.length).height;
+
+    let stepHor = 256;
+    let stepVer = 256;
+    let stepHorNow = 0;
+    let stepVerNow = 0;
+    let c = 0;
+
+    for (let i = 0; i < matrix(this.images.length).height; i++) {
+      stepHorNow = i * stepHor;
+      for (let k = 0; k < matrix(this.images.length).width; k++) {
+        stepVerNow = k * stepVer;
+        const ctx = canvas.getContext('2d');
+        try {
+          if (this.images[c]) {
+            ctx.drawImage(this.images[c], stepHorNow, stepVerNow);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+        c++;
+      }
+    }
+
+    this.imageElement.src = canvas.toDataURL();
+    this.spriteURL = this.imageElement.src;
+    this.createCode();
+
+    function matrix(length) {
+      let sqr = Math.ceil(Math.sqrt(length));
+      console.log(sqr);
+      return {
+        width: sqr,
+        height: sqr
+      };
+    }
+  }
+  createCode() {
+    console.log('CREATE CODE');
+    let text = '.icon {\ndisplay: inline-block;\nbackground-image: url(' + this.spriteURL + ');\n}\n.icon_like {\nbackground-position: -48px 0;\nwidth: 50px;\nheight: 50px;\n}';
+    this.codeContainer.innerHTML = text;
+  }
+}
+
+function cons() {
+  console.log('TEST');
+}
+
+function timeout(callback) {
+  setTimeout(() => {
+    return callback();
+  }, 2)
 }
 
 const spriteGenerator = new SpriteGenerator(document.getElementById('generator'));
